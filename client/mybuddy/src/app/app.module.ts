e9bb04ffb788497a4dc0f9c86b2a7c5d50483b4c
Kaan <furkankaanulucay@gmail.com>
2022-02-07 12:22:29 +0300

    register component created

--- client/mybuddy/src/app/app.module.ts
+++ client/mybuddy/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogInPageComponent } from './modules/auth/pages/log-in-page/log-in-page.component';
+import { RegisterPageComponent } from './modules/auth/pages/register-page/register-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInPageComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
