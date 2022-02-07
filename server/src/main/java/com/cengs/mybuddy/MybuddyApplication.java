388f598608258c1385903221458b822a451f3995
mertinc <mertincesu1999@hotmail.com>
2022-02-07 11:42:57 +0300

    server side initialized.

---
+++ server/src/main/java/com/cengs/mybuddy/MybuddyApplication.java
@@ -0,0 +1,13 @@
+package com.cengs.mybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybuddyApplication.class, args);
+		System.out.println("App working");
+	}
+}
