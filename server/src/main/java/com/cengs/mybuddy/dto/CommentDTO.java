585f2f408adc38d276e0bcaff66897c2b10640b5
Zeynep Gok <zeynep.gk3@gmail.com>
2022-02-08 15:32:52 +0300

    created addComment function

---
+++ server/src/main/java/com/cengs/mybuddy/dto/CommentDTO.java
@@ -0,0 +1,34 @@
+package com.cengs.mybuddy.dto;
+
+import java.util.Date;
+import java.util.UUID;
+
+import javax.validation.constraints.NotNull;
+
+import com.cengs.mybuddy.model.User;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.data.annotation.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CommentDTO {
+    @Id
+    private UUID id;
+    @NotNull(message = "user is mandatory")
+    private UUID userId;
+    //@NotNull(message = "name is mandatory")
+    //private String name;
+    @NotNull(message = "date is mandatory")
+    @JsonFormat(pattern="yyyy-MM-dd",shape= JsonFormat.Shape.STRING)
+    private String date;
+    @NotNull(message = "content is mandatory")
+    private String content;
+    //@Id
+    //private UUID id;
+}
---
+++ server/src/main/java/com/cengs/mybuddy/service/CommentService.java
@@ -0,0 +1,55 @@
+package com.cengs.mybuddy.service;
+
+import com.cengs.mybuddy.dto.CommentDTO;
+import com.cengs.mybuddy.dto.UserDTO;
+import com.cengs.mybuddy.model.Comment;
+import com.cengs.mybuddy.model.User;
+import com.cengs.mybuddy.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class CommentService {
+    @Autowired
+    private UserService userService;
+    private final CommentRepository commentRepository;
+
+
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    public CommentDTO createComment(CommentDTO commentDTO){
+        Comment newComment=new Comment();
+        newComment=mapCommentDtoToComment(commentDTO);
+        newComment.setUser(userService.findById(commentDTO.getUserId()));
+        newComment.setId(UUID.randomUUID());
+        newComment=commentRepository.save(newComment);
+        return mapCommentDtoToComment(newComment);
+    }
+
+
+    private Comment mapCommentDtoToComment(CommentDTO dto) {
+        Comment comment = new Comment();
+        comment.setId(dto.getId());
+        comment.setUser(userService.findById(dto.getUserId()));
+        comment.setDate(dto.getDate());
+        comment.setContent(dto.getContent());
+        return comment;
+    }
+
+    private CommentDTO mapCommentDtoToComment(Comment comment) {
+        CommentDTO dto = new CommentDTO();
+        dto.setId(comment.getId());
+        System.out.println(comment.getUser());
+        dto.setUserId(comment.getUser().getId());
+        dto.setDate(comment.getDate());
+        dto.setContent(comment.getContent());
+        return dto;
+    }
+}
