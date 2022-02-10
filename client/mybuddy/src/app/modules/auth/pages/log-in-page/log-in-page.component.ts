26207c2c6912378679c2ef61af0266d3cc2d35fd
Kaan <furkankaanulucay@gmail.com>
2022-02-10 16:54:30 +0300

    login page updated

--- client/mybuddy/src/app/modules/auth/pages/log-in-page/log-in-page.component.ts
+++ client/mybuddy/src/app/modules/auth/pages/log-in-page/log-in-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiHttpService } from 'src/app/shared/services/api-http.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-log-in-page',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogInPageComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private apiService:ApiHttpService, private fb: FormBuilder, private authService:AuthService) { }
+  loginForm: FormGroup;
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  login(formData: FormGroup){
+    console.log(formData.value);
+
+    this.apiService.login(formData.value).then((data) => {
+      console.log(data);
+      if(data === true) {
+        this.authService.user$.subscribe(value => {
+          value = ;
+          console.log(value);
+        })
+      }
+    }).catch((e) => {
+      console.log(e);
+    })
   }
 
 }
